d42f1d20f9cbc1a8fcc3f6b8ee4c887a3a7fb7ac
ashishmisal23 <ashishmisal23@gmail.com>
2024-04-11 02:01:37 +0530

    first commit

---
+++ src/pages/EditNews.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const EditNews = () => {
+  return (
+    <Layout>
+      EditNews
+    </Layout>
+  )
+}
+
+export default EditNews
---
+++ src/pages/PostedNewsItems.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+import Layout from '../components/Layout';
+import Spinner from '../components/Spinner';
+
+const PostedNewsItems = () => {
+  const [userData, setUserData] = useState(null); // Initialize with null or {}
+  const [loading, setLoading] = useState(false);
+  const [newsItems, setNewsItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        // Retrieve user data from localStorage
+        const user = localStorage.getItem('user');
+        console.log(user);
+        if (user) {
+          setUserData({ email: user }); // Set userData directly to the email retrieved from localStorage
+          const response = await axios.post(
+            `http://localhost:5000/api/getnewsitemsbyemail/${user}`
+          );
+          setNewsItems(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching news items:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Layout>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div>
+          {newsItems.map((item) => (
+            <div key={item._id} className="p-5">
+              <h1 className="my-3 text-2xl font-semibold">{item.title}</h1>
+              <hr />
+              <p>{item.description}</p>
+              <hr />
+              <p>{item.content}</p>
+              <hr />
+              <p>{item.postedBy.email}</p>
+              <hr />
+            </div>
+          ))}
+        </div>
+      )}
+    </Layout>
+  );
+};
+
+export default PostedNewsItems;
