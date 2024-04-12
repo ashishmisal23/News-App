b3e18e691cda35383ccb08da6812de479006adba
ashishmisal23 <ashishmisal23@gmail.com>
2024-04-12 15:36:05 +0530

    Changes Done

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 import Spinner from '../components/Spinner'
+import profileImg from './images/profile.png'
 
 const Profile = () => {
   const [userData, setUserData] = useState({});
@@ -28,10 +29,18 @@ const Profile = () => {
       {loading ? (
         <Spinner />
       ) : (
-        <div className="p-5">
-          <h1>{userData ? `Name: ${userData.name}` : ("User Details not available Found")}</h1>
-          <h1>{userData ? `Email: ${userData.email}` : (" ")}</h1>
-
+        <div href="#" class="flex flex-col items-center bg-[#145c2aaf] text-yellow-200 font-bold border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl">
+          <img class="object-cover rounded-t-lg md:d:rounded-none md:rounded-s-lg"
+            src={profileImg}
+            alt="User Image" />
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-white-900 dark:text-white">
+              {userData ? `Name: ${userData.name}` : ("User Details not available Found")}
+            </h5>
+            <p class="mb-3 font-normal text-white">
+              {userData ? `Email: ${userData.email}` : (" ")}
+            </p>
+          </div>
         </div>
       )}
     </Layout>
